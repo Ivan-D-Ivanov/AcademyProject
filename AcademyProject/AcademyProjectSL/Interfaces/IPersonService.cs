b1edde840d1aaf039970ff7226c108c92fff7201
i.ivan <i.ivanov@draftkings.com>
2022-10-03 09:43:16 +0300

    Change in interface

--- AcademyProject/AcademyProjectSL/Interfaces/IPersonService.cs
+++ AcademyProject/AcademyProjectSL/Interfaces/IPersonService.cs
@@ -5,7 +5,6 @@ namespace AcademyProjectSL.Interfaces
     public interface IPersonService
     {
         IEnumerable<Person> GetAllPersons { get; }
-
         Person? AddPerson(Person user);
         Person? DeletePerson(int userId);
         Person? GetById(int id);
