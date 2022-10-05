da155780e58ac66337d700ee7128c8d5d1604b56
i.ivan <i.ivanov@draftkings.com>
2022-10-05 12:02:37 +0300

    HealthChecks implemented

--- AcademyProject/AcademyProject/Program.cs
+++ AcademyProject/AcademyProject/Program.cs
@@ -1,8 +1,9 @@
 using AcademyProject.Extensions;
+using AcademyProject.HealthChecks;
 using FluentValidation;
 using FluentValidation.AspNetCore;
-using Serilog;
 using Serilog.Sinks.SystemConsole.Themes;
+using Serilog;
 
 var logger = new LoggerConfiguration()
     .Enrich.FromLogContext()
@@ -15,17 +16,21 @@ builder.Logging.AddSerilog(logger);
 
 // Add services to the container.
 builder.Services.RegisterRepositoriesPerson()
-    .RegisterServicePerson()
-    .AddAutoMapper(typeof(Program));
+                .RegisterServicePerson()
+                .AddAutoMapper(typeof(Program));
 
 builder.Services.AddFluentValidationAutoValidation()
-    .AddFluentValidationClientsideAdapters();
+                .AddFluentValidationClientsideAdapters();
 builder.Services.AddValidatorsFromAssemblyContaining(typeof(Program));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHealthChecks()
+    .AddCheck<SqlHealthCheck>("Sql Server")
+    .AddUrlGroup(new Uri("https://google.bg"), name: "Google Connection");
 
 var app = builder.Build();
 
@@ -41,5 +46,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+//app.MapHealthChecks("/health");
+app.RegisterHealthChecks();
 
 app.Run();
---
+++ AcademyProject/AcademyProjectModels/HealthChecks/IndividualHealthCheckResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace AcademyProjectModels.HealthChecks
+{
+    public class IndividualHealthCheckResponse
+    {
+        public string Status { get; init; }
+
+        public string Component { get; init; }
+
+        public string Description { get; init; }
+    }
+}
