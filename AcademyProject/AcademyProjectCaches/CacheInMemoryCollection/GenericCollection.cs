689e1e4c7b58739960b967818d4e958c18fc337b
i.ivan <i.ivanov@draftkings.com>
2022-10-14 16:38:02 +0300

    KafkaGenericConsumer implemented

---
+++ AcademyProject/AcademyProjectCaches/CacheInMemoryCollection/GenericCollection.cs
@@ -0,0 +1,18 @@
+﻿namespace AcademyProjectCaches.CacheInMemoryCollection
+{
+    public class GenericCollection<TValaue>
+    {
+        private static List<TValaue> _list = new List<TValaue>();
+
+        public async Task<IEnumerable<TValaue>> GetAllItems()
+        {
+            return _list;
+        }
+
+        public TValaue Add(TValaue value)
+        {
+            _list.Add(value);
+            return value;
+        }
+    }
+}
